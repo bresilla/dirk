55251b1d626973518e1ff437c7b2804619530533
Trim Bresilla <trim.bresilla@gmail.com>
2019-01-12 19:28:26 +0100

    file encryption created

---
+++ crypt.go
@@ -0,0 +1,145 @@
+//source https://github.com/isfonzar/filecrypt/blob/master/filecrypt.go
+package dirk
+
+import (
+	"bytes"
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/hmac"
+	"crypto/rand"
+	"crypto/sha1"
+	"encoding/hex"
+	"hash"
+	"io"
+	"io/ioutil"
+	"os"
+)
+
+func Encrypt(source string, password []byte) {
+
+	if _, err := os.Stat(source); os.IsNotExist(err) {
+		panic(err.Error())
+	}
+
+	plaintext, err := ioutil.ReadFile(source)
+
+	if err != nil {
+		panic(err.Error())
+	}
+
+	key := password
+	nonce := make([]byte, 12)
+
+	// Randomizing the nonce
+	if _, err := io.ReadFull(rand.Reader, nonce); err != nil {
+		panic(err.Error())
+	}
+
+	dk := Key(key, nonce, 4096, 32, sha1.New)
+
+	block, err := aes.NewCipher(dk)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	aesgcm, err := cipher.NewGCM(block)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	ciphertext := aesgcm.Seal(nil, nonce, plaintext, nil)
+
+	// Append the nonce to the end of file
+	ciphertext = append(ciphertext, nonce...)
+
+	f, err := os.Create(source)
+	if err != nil {
+		panic(err.Error())
+	}
+	_, err = io.Copy(f, bytes.NewReader(ciphertext))
+	if err != nil {
+		panic(err.Error())
+	}
+}
+
+func Decrypt(source string, password []byte) {
+
+	if _, err := os.Stat(source); os.IsNotExist(err) {
+		panic(err.Error())
+	}
+
+	ciphertext, err := ioutil.ReadFile(source)
+
+	if err != nil {
+		panic(err.Error())
+	}
+
+	key := password
+	salt := ciphertext[len(ciphertext)-12:]
+	str := hex.EncodeToString(salt)
+
+	nonce, err := hex.DecodeString(str)
+
+	dk := Key(key, nonce, 4096, 32, sha1.New)
+
+	block, err := aes.NewCipher(dk)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	aesgcm, err := cipher.NewGCM(block)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	plaintext, err := aesgcm.Open(nil, nonce, ciphertext[:len(ciphertext)-12], nil)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	f, err := os.Create(source)
+	if err != nil {
+		panic(err.Error())
+	}
+	_, err = io.Copy(f, bytes.NewReader(plaintext))
+	if err != nil {
+		panic(err.Error())
+	}
+}
+
+func Key(password, salt []byte, iter, keyLen int, h func() hash.Hash) []byte {
+	prf := hmac.New(h, password)
+	hashLen := prf.Size()
+	numBlocks := (keyLen + hashLen - 1) / hashLen
+
+	var buf [4]byte
+	dk := make([]byte, 0, numBlocks*hashLen)
+	U := make([]byte, hashLen)
+	for block := 1; block <= numBlocks; block++ {
+		// N.B.: || means concatenation, ^ means XOR
+		// for each block T_i = U_1 ^ U_2 ^ ... ^ U_iter
+		// U_1 = PRF(password, salt || uint(i))
+		prf.Reset()
+		prf.Write(salt)
+		buf[0] = byte(block >> 24)
+		buf[1] = byte(block >> 16)
+		buf[2] = byte(block >> 8)
+		buf[3] = byte(block)
+		prf.Write(buf[:4])
+		dk = prf.Sum(dk)
+		T := dk[len(dk)-hashLen:]
+		copy(U, T)
+
+		// U_n = PRF(password, U_(n-1))
+		for n := 2; n <= iter; n++ {
+			prf.Reset()
+			prf.Write(U)
+			U = U[:0]
+			U = prf.Sum(U)
+			for x := range U {
+				T[x] ^= U[x]
+			}
+		}
+	}
+	return dk[:keyLen]
+}
---
+++ progress.go
@@ -0,0 +1,204 @@
+//inspired by https://github.com/briandowns/spinner
+package dirk
+
+import (
+	"encoding/hex"
+	"fmt"
+	"io"
+	"os"
+	"runtime"
+	"sync"
+	"time"
+	"unicode/utf8"
+
+	"github.com/fatih/color"
+)
+
+var SpinnerSets = map[int][]string{
+	0:  {"←", "↖", "↑", "↗", "→", "↘", "↓", "↙"},
+	1:  {"▁", "▃", "▄", "▅", "▆", "▇", "█", "▇", "▆", "▅", "▄", "▃", "▁"},
+	2:  {"▖", "▘", "▝", "▗"},
+	3:  {"┤", "┘", "┴", "└", "├", "┌", "┬", "┐"},
+	4:  {"◢", "◣", "◤", "◥"},
+	5:  {"◰", "◳", "◲", "◱"},
+	6:  {"◴", "◷", "◶", "◵"},
+	7:  {"◐", "◓", "◑", "◒"},
+	8:  {".", "o", "O", "@", "*"},
+	9:  {"|", "/", "-", "\\"},
+	10: {"◡◡", "⊙⊙", "◠◠"},
+	11: {"⣾", "⣽", "⣻", "⢿", "⡿", "⣟", "⣯", "⣷"},
+	12: {">))'>", " >))'>", "  >))'>", "   >))'>", "    >))'>", "   <'((<", "  <'((<", " <'((<"},
+	13: {"⠁", "⠂", "⠄", "⡀", "⢀", "⠠", "⠐", "⠈"},
+	14: {"⠋", "⠙", "⠹", "⠸", "⠼", "⠴", "⠦", "⠧", "⠇", "⠏"},
+	15: {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"},
+	16: {"▉", "▊", "▋", "▌", "▍", "▎", "▏", "▎", "▍", "▌", "▋", "▊", "▉"},
+	17: {"■", "□", "▪", "▫"},
+	18: {"←", "↑", "→", "↓"},
+	19: {"╫", "╪"},
+	20: {"⇐", "⇖", "⇑", "⇗", "⇒", "⇘", "⇓", "⇙"},
+	21: {"⠁", "⠁", "⠉", "⠙", "⠚", "⠒", "⠂", "⠂", "⠒", "⠲", "⠴", "⠤", "⠄", "⠄", "⠤", "⠠", "⠠", "⠤", "⠦", "⠖", "⠒", "⠐", "⠐", "⠒", "⠓", "⠋", "⠉", "⠈", "⠈"},
+	22: {"⠈", "⠉", "⠋", "⠓", "⠒", "⠐", "⠐", "⠒", "⠖", "⠦", "⠤", "⠠", "⠠", "⠤", "⠦", "⠖", "⠒", "⠐", "⠐", "⠒", "⠓", "⠋", "⠉", "⠈"},
+	23: {"⠁", "⠉", "⠙", "⠚", "⠒", "⠂", "⠂", "⠒", "⠲", "⠴", "⠤", "⠄", "⠄", "⠤", "⠴", "⠲", "⠒", "⠂", "⠂", "⠒", "⠚", "⠙", "⠉", "⠁"},
+	24: {"⠋", "⠙", "⠚", "⠒", "⠂", "⠂", "⠒", "⠲", "⠴", "⠦", "⠖", "⠒", "⠐", "⠐", "⠒", "⠓", "⠋"},
+	25: {"ｦ", "ｧ", "ｨ", "ｩ", "ｪ", "ｫ", "ｬ", "ｭ", "ｮ", "ｯ", "ｱ", "ｲ", "ｳ", "ｴ", "ｵ", "ｶ", "ｷ", "ｸ", "ｹ", "ｺ", "ｻ", "ｼ", "ｽ", "ｾ", "ｿ", "ﾀ", "ﾁ", "ﾂ", "ﾃ", "ﾄ", "ﾅ", "ﾆ", "ﾇ", "ﾈ", "ﾉ", "ﾊ", "ﾋ", "ﾌ", "ﾍ", "ﾎ", "ﾏ", "ﾐ", "ﾑ", "ﾒ", "ﾓ", "ﾔ", "ﾕ", "ﾖ", "ﾗ", "ﾘ", "ﾙ", "ﾚ", "ﾛ", "ﾜ", "ﾝ"},
+	26: {".", "..", "..."},
+	27: {"▁", "▂", "▃", "▄", "▅", "▆", "▇", "█", "▉", "▊", "▋", "▌", "▍", "▎", "▏", "▏", "▎", "▍", "▌", "▋", "▊", "▉", "█", "▇", "▆", "▅", "▄", "▃", "▂", "▁"},
+	28: {".", "o", "O", "°", "O", "o", "."},
+	29: {"+", "x"},
+	30: {"v", "<", "^", ">"},
+	31: {"🌍", "🌎", "🌏"},
+	32: {"◜", "◝", "◞", "◟"},
+	33: {"⬒", "⬔", "⬓", "⬕"},
+	34: {"⬖", "⬘", "⬗", "⬙"},
+	35: {"♠", "♣", "♥", "♦"},
+}
+
+// Spinner struct to hold the provided options
+type Spinner struct {
+	Percent    int
+	Delay      time.Duration                 // Delay is the speed of the indicator
+	chars      []string                      // chars holds the chosen character set
+	Prefix     string                        // Prefix is the text preppended to the indicator
+	Suffix     string                        // Suffix is the text appended to the indicator
+	lastOutput string                        // last character(set) written
+	color      func(a ...interface{}) string // default color is white
+	lock       *sync.RWMutex                 //
+	Writer     io.Writer                     // to make testing better, exported so users have access
+	active     bool                          // active holds the state of the spinner
+	stopChan   chan struct{}                 // stopChan is a channel used to stop the indicator
+}
+
+// New provides a pointer to an instance of Spinner with the supplied options
+func New(cs []string, d time.Duration, i int) *Spinner {
+	s := &Spinner{
+		Delay:    d,
+		Percent:  i,
+		chars:    cs,
+		color:    color.New(color.FgWhite).SprintFunc(),
+		lock:     &sync.RWMutex{},
+		Writer:   color.Output,
+		active:   false,
+		stopChan: make(chan struct{}, 1),
+	}
+	return s
+}
+
+// Active will return whether or not the spinner is currently active
+func (s *Spinner) Active() bool {
+	return s.active
+}
+
+// Start will start the indicator
+func (s *Spinner) Start() {
+	s.lock.Lock()
+	if s.active {
+		s.lock.Unlock()
+		return
+	}
+	s.active = true
+	s.lock.Unlock()
+
+	go func() {
+		for {
+			for i := 0; i < len(s.chars); i++ {
+				select {
+				case <-s.stopChan:
+					return
+				default:
+					s.lock.Lock()
+					s.erase()
+					var outColor string
+					if runtime.GOOS == "windows" {
+						if s.Writer == os.Stderr {
+							outColor = fmt.Sprintf("\r%s%s%s ", s.Prefix, s.chars[i], s.Suffix)
+						} else {
+							outColor = fmt.Sprintf("\r%s%s%s ", s.Prefix, s.color(s.chars[i]), s.Suffix)
+						}
+					} else {
+						outColor = fmt.Sprintf("%s%s%s ", s.Prefix, s.color(s.chars[i]), s.Suffix)
+					}
+					outPlain := fmt.Sprintf("%s%s%s ", s.Prefix, s.chars[i], s.Suffix)
+					fmt.Fprint(s.Writer, outColor)
+					s.lastOutput = outPlain
+					delay := s.Delay
+					s.lock.Unlock()
+
+					time.Sleep(delay)
+				}
+			}
+		}
+	}()
+}
+
+// Stop stops the indicator
+func (s *Spinner) Stop() {
+	s.lock.Lock()
+	defer s.lock.Unlock()
+	if s.active {
+		s.active = false
+		s.erase()
+		s.stopChan <- struct{}{}
+	}
+}
+
+// Restart will stop and start the indicator
+func (s *Spinner) Restart() {
+	s.Stop()
+	s.Start()
+}
+
+// Reverse will reverse the order of the slice assigned to the indicator
+func (s *Spinner) Reverse() {
+	s.lock.Lock()
+	defer s.lock.Unlock()
+	for i, j := 0, len(s.chars)-1; i < j; i, j = i+1, j-1 {
+		s.chars[i], s.chars[j] = s.chars[j], s.chars[i]
+	}
+}
+
+// Color will set the struct field for the given color to be used
+func (s *Spinner) Color(colors ...string) error {
+	colorAttributes := make([]color.Attribute, len(colors))
+	s.color = color.New(colorAttributes...).SprintFunc()
+	s.Restart()
+	return nil
+}
+
+// UpdateSpeed will set the indicator delay to the given value
+func (s *Spinner) UpdateSpeed(d time.Duration) {
+	s.lock.Lock()
+	defer s.lock.Unlock()
+	s.Delay = d
+}
+
+// UpdateCharSet will change the current character set to the given one
+func (s *Spinner) UpdateCharSet(cs []string) {
+	s.lock.Lock()
+	defer s.lock.Unlock()
+	s.chars = cs
+}
+
+// erase deletes written characters
+func (s *Spinner) erase() {
+	n := utf8.RuneCountInString(s.lastOutput)
+	if runtime.GOOS == "windows" {
+		clearString := "\r"
+		for i := 0; i < n; i++ {
+			clearString += " "
+		}
+		fmt.Fprintf(s.Writer, clearString)
+		return
+	}
+	del, _ := hex.DecodeString("7f")
+	for _, c := range []string{
+		"\b",
+		string(del),
+		"\b",
+		"\033[K", // for macOS Terminal
+	} {
+		for i := 0; i < n; i++ {
+			fmt.Fprintf(s.Writer, c)
+		}
+	}
+	s.lastOutput = ""
+}
