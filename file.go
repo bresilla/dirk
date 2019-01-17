e78e2f6f18d87a3efe15762582e3afb6829f11ed
Trim Bresilla <trim.bresilla@gmail.com>
2019-01-17 14:57:38 +0100

    inside file search (with RegExp)

--- file.go
+++ file.go
@@ -432,3 +432,24 @@ func chooseFile(incFolder, incFiles, incHidden, recurrent bool, dir File) (list 
 	}
 	return
 }
+
+type Files []File
+
+func (e Files) String(i int) string    { return e[i].Name }
+func (e Files) Len() int               { return len(e) }
+func (e Files) Swap(i, j int)          { e[i], e[j] = e[j], e[i] }
+func (e Files) Less(i, j int) bool     { return e[i].Name[1:] < e[j].Name[1:] }
+func (e Files) SortSize(i, j int) bool { return e[i].Size < e[j].Size }
+func (e Files) SortDate(i, j int) bool { return e[i].BrtTime.Before(e[j].BrtTime) }
+
+func MakeFiles(path []string) (files Files, err error) {
+	files = Files{}
+	for i := range path {
+		if file, err := MakeFile(path[i]); err != nil {
+			return files, err
+		} else {
+			files = append(files, file)
+		}
+	}
+	return files, nil
+}
